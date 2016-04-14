9c1f69577dd03b262d2da806d72b421c0a13abe1
Nicolas <nick11011@gmail.com>
2016-04-14 13:31:19 +0000

    Minha soma

---
+++ soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, soma;
+    
+    num1 = 10;
+    num2 = 9;
+    
+    soma = num1 + num2;
+    
+    printf("\nA soma de num1+num2 e %d\n", soma);
+    
+    return 0;
+}
