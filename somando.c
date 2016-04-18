2dd3782e1e5fe16becfdb823d2be9c72172e8249
Nicolas <nick11011@gmail.com>
2016-04-18 18:44:54 +0000

    somando

--- somando.c
+++ somando.c
@@ -2,15 +2,20 @@
 
 int main()
 {
-    int num1, num2, soma;
+    int num1, num2, num3, soma;
     
-    printf("\nEntre com num1:");
+    printf("\nEntre com o número 1\n", num1);
     scanf("%d", &num1);
     
-    printf("\n Entre com num2");
+    printf("\nEntre com o num2\n", num2);
     scanf("%d", &num2);
     
-    soma = num1 + num2;
- 
-    printf("\nSoma = %d\n", soma);   
+    printf("\nEntre com o último número\n", num3);
+    scanf("%d", &num3);
+    
+    soma = num1 + num2 - num3;
+    
+    printf("\nA soma é =%d\n", soma);
+    
+    return 0;
 }
