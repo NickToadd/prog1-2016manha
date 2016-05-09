7c24f18cbc8eaf03ee0147941edcbdef2271fd82
Nicolas <nick11011@gmail.com>
2016-05-09 13:09:24 +0000

    media com variavel2

--- variavel.c
+++ variavel.c
@@ -3,9 +3,10 @@
 int main()
 {
     
- int i;
+ int i, qtdMaior=0;
  float numeros[10];
  float soma=0, media=0;
+ float maior=-999;
  
  
  for(i=0;i<=9;i++)
@@ -13,6 +14,11 @@ int main()
      printf("\nEntre com a nota do aluno %d\n", i+1);
      scanf("%f", &numeros[i]);
      soma = soma + numeros[i];
+     
+     if(numeros[i] > maior)
+     {
+         maior = numeros[i];
+     }
  }
     
     media = soma / i;
@@ -22,9 +28,19 @@ int main()
     {
        if(numeros[i]>=media)
     {
-        printf("\nMaior media %.2f", numeros[i]);
+        printf("\nMaior media %.2f\n", numeros[i]);
     }
+    if(numeros[i] == maior)
+    {
+        qtdMaior++;
     }
+    
+    }
+    
+    
+    printf("\n%d alunos tiraram a maior nota = %.2f", qtdMaior, maior);
+    
+    
     
     return 0;
 }
