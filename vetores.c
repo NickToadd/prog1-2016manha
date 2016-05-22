72911bc377f5286eef51ffe1de8f39c2d8e4a66b
Nicolas <nick11011@gmail.com>
2016-05-22 14:13:17 +0000

    Meu trabalho da av2

--- vetores.c
+++ vetores.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-    float media, soma=0;
+    
+    printf("\nOlá, meu nome é Nícolas Serrano e este programa pode ser visto no seguinte endereço do github: https://github.com/seu_usuario/seu_repositorio/prog1_av2_1.c\n")
+    
+    float media, soma=0, av1, av2, av3;
     int i,j;
     float notas[4][3]; //coluna =, 1 e 2 = av1, av3 e av3
     
@@ -14,9 +17,6 @@ int main()
             printf("\nEntre com a av%d do aluno %d: ", j+1, i+1);
             scanf("%f", &notas[i][j]);
             }
-            
-            
-            
     }
 
     printf("\n");
@@ -47,5 +47,33 @@ int main()
         printf("\nMédia\t\t%.2f", media);
         printf("\n------------------------------------");
         
+        printf("\nEntre com a matricula[1-4]:");
+        scanf("%d", &i);
+        
+        i--;
+        
+        printf("\n Nome do aluno\tAV1\tAV2\tAV3");
+        printf("\n------------------------------------");
+        printf("\nAluno %d\t", i+1);
+        for(j=0; j<=2; j++)
+        {
+            printf("\t%.2f", notas[i][j]);
+        }
+        
+        av1 = notas[i][0];
+        av2 = notas[i][1];
+        av3 = notas[i][2];
+        
+        if(  (av1 + av2) /2 >=6 ||
+             (av1 + av3) /2 >=6 ||
+             (av1 + av3) /2 >=6  )
+             
+        {
+            printf("\t\nAPROVADO\n");
+        }     
+        else
+        {
+            printf("\t\nREPROVADO\n");
+        }
     return 0;
 }
